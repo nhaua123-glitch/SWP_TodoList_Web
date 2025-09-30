c918a3a1d115bcd7f7bb4230b2fa943fb515ef95
vietbach17 <vietbach171205@gmail.com>
2025-09-30 09:24:27 +0700

    Thêm chức năng sign up cho project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -88,15 +88,15 @@ export default function DashboardPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
             <div className="bg-white border border-gray-200 rounded-xl p-6 shadow-md hover:shadow-lg transition">
-              <h3 className="text-lg font-semibold mb-2 text-gray-700">muc tieu trong ngay</h3>
+              <h3 className="text-lg font-semibold mb-2 text-gray-700">Add todolist</h3>
             </div>
 
             <div className="bg-white border border-gray-200 rounded-xl p-6 shadow-md hover:shadow-lg transition">
-              <h3 className="text-lg font-semibold mb-2 text-gray-700">muc tieu trong thang</h3>
+              <h3 className="text-lg font-semibold mb-2 text-gray-700">remove todolist</h3>
             </div>
 
             <div className="bg-white border border-gray-200 rounded-xl p-6 shadow-md hover:shadow-lg transition">
-              <h3 className="text-lg font-semibold mb-2 text-gray-700">muc tieu trong nam</h3>
+              <h3 className="text-lg font-semibold mb-2 text-gray-700">goal todolist in month</h3>
             </div>
             
           </div>
