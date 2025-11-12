3a45cd748b0d761cbd3b1a8fc360fd38d2939088
nhaua123-glitch <nhaua123@gmail.com>
2025-11-12 18:24:41 +0700

    fixall

--- app/api/public/users/[id]/route.ts
+++ app/api/public/users/[id]/route.ts
@@ -28,16 +28,16 @@ const supabase = createServerClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     cookies: {
-  get(name: string) {
-    return cookieStore.get(name)?.value
-  },
-  set(name: string, value: string, options: CookieOptions) { // <-- Đã sửa
-    cookieStore.set({ name, value, ...options })
-  },
-  remove(name: string, options: CookieOptions) { // <-- Đã sửa
-    cookieStore.set({ name, value: '', ...options })
-  },
-},
+      async get(name: string) {
+        return (await cookieStore).get(name)?.value
+      },
+      async set(name: string, value: string, options) {
+        (await cookieStore).set({ name, value, ...options })
+      },
+      async remove(name: string, options) {
+        (await cookieStore).set({ name, value: '', ...options })
+      },
+    },
   }
 )
     const {
